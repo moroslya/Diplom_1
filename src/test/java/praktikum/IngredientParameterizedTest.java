3f8fa7ade3dc0bdf6476324a14e2d71c76b51d72
Eugenia D <moroslya@yandex.ru>
2022-08-27 00:28:25 +0300

    add_diploma_project_files

---
+++ src/test/java/praktikum/IngredientParameterizedTest.java
@@ -0,0 +1,40 @@
+package praktikum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static praktikum.IngredientType.*;
+
+@RunWith(Parameterized.class)
+public class IngredientParameterizedTest {
+
+    private final IngredientType setIngredientType;
+
+    public IngredientParameterizedTest(IngredientType setIngredientType) {
+
+        this.setIngredientType = setIngredientType;
+
+    }
+
+    @Test
+    public void getIngredientTypeReturnCorrectValue() {
+
+        Ingredient ingredient = new Ingredient(setIngredientType, "Кисло-сладкий соус", 100);
+
+        IngredientType actualIngredientType = ingredient.getType();
+
+        Assert.assertEquals(setIngredientType, actualIngredientType);
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                {SAUCE},
+                {FILLING}
+        };
+    }
+
+}
